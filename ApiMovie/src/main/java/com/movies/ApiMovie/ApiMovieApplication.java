4ead0af82b669e41ef1fce55fd7841fcbe83ead3
Donkross360 <mart2great@yahoo.com>
2023-03-28 14:41:36 +0100

    frontend & Backend

---
+++ ApiMovie/src/main/java/com/movies/ApiMovie/ApiMovieApplication.java
@@ -0,0 +1,17 @@
+package com.movies.ApiMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiMovieApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApiMovieApplication.class, args);
+	}
+
+
+
+}
---
+++ ApiMovie/src/test/java/com/movies/ApiMovie/ApiMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.ApiMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
